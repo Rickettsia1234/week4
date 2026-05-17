886d5b97a578a6c49b720d7664d7f8a825579e01
Rickettsia1234 <ej100504@naver.com>
2026-05-17 20:52:23 +0900

    첫번째 액티비티

--- app/src/main/java/com/example/week4/MainActivity.kt
+++ app/src/main/java/com/example/week4/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.example.week4
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var count: Int = 0
+    lateinit var textView_count: TextView//지연 초기화로 나중에 선언 하게 함
+    val activityResultLauncher = registerForActivityResult(//콜백 통로 설정하는 함수
+    /*
+        registerForActivityResult :
+            결과를 받기 위한 액티비티를 등록하는 함수
+            생명주기에서 현재 액티비티를 지우지 못하게 하기 위함
+        ActivityResultContracts :
+            액티비티 간 상호작용 시 타입 안정성을 위한 추상화된 계약 인터페이스 집합
+            ActivityResultContracts :
+                다른 액티비티를 지정하고, 데이터를 돌려받는 계약 형식
+    */
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->//다음 화면이 닫히면 해당 블록을 실행함
+        if(result.resultCode == RESULT_OK) {
+            count = result.data?.getIntExtra("RETURN_COUNT", 0) ?: 0
+            //엘비스 연산자를 사용해서 0을 기본값으로 가짐
+            textView_count.text = count.toString()
+        }
+    }
+
+    fun showToastMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun changeActivityWithCount() {
+        val intent = Intent(this, MainActivity2::class.java)
+        //intent에 출발지, 목적지를 넣는다.
+        intent.putExtra("INTENT_COUNT", count)
+        //이름, 변수를 묶어 맵 자료구조와 유사한 Bundle 자료구조에 넣는다
+        activityResultLauncher.launch(intent)
+        //intent를 발송한다
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +56,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        textView_count = findViewById<TextView>(R.id.textView_count)
+        val button_count = findViewById<Button>(R.id.button_count)//var 이 아니라 val 사용
+        /*
+            findViewById : id를 이용해 주소값을 찾아 대입하는 함수
+            <컴포넌트 타입> (id) R : 리소스의 약자, 모든 리소스 id : 고유 식별자
+            Alt + Enter 로 모듈 가져올 수 있음
+         */
+        val button_toast = findViewById<Button>(R.id.button_toast)
+        val button_random = findViewById<Button>(R.id.button_random)
+
+        button_count.setOnClickListener {//함수의 마지막 인자가 람다식일 경우{} 소괄호 생략 가능
+            count++
+            textView_count.text = count.toString()
+        }
+
+        button_toast.setOnClickListener {
+            showToastMessage("toast message")
+        }
+
+        button_random.setOnClickListener {
+            changeActivityWithCount()
+        }
+
     }
 }
